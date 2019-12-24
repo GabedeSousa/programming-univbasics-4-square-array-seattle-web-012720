555000702c8b620af33b8eed4e915c6039d7cea2
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2019-12-24 00:53:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,9 @@
 
   
   def square_array(numbers)
-    count = 0
     numbers = [1,2,3]
+    count = 0
     while count < numbers.lenght do
       numbers.index ** 2 
     end
-    square_array(numbers)
-  end
-  
-    
     
